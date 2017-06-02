3580ac162814374d920c2e3ba2b36e3a6b7f6044
robyncastro <robyny3k@gmail.com>
2017-06-02 07:54:17 -0700

    LEDS are now blinking

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/ROSArduinoBridge.ino
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/ROSArduinoBridge.ino
@@ -159,6 +159,19 @@ long arg2;
 //    In this case, we send it a request character, and use that to send motor commands
 const int RC_PIN = 6;
 
+// constants won't change. Used here to set a pin number :
+const int ledPin =  13;// the number of the LED pin
+
+// Variables will change :
+int ledState = LOW;             // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change :
+const long BLINK_INTERVAL = 1000;           // interval at which to blink (milliseconds)
+
 // Size of payload being sent from the RC_Slave Arduino
 const int BUFFER_SIZE = 10;
 
@@ -288,7 +301,7 @@ int runCommand() {
 /* Setup function--runs once at startup. */
 void setup() {
   Serial.begin(BAUDRATE);
-
+  pinMode(ledPin, OUTPUT);
 // Initialize the motor controller if used */
 #ifdef USE_BASE
 #ifdef ARDUINO_ENC_COUNTER
@@ -338,6 +351,28 @@ void loop() {
   // Sets the robot state depending on RC_Slave input pin
   getRobotState();
   if (current_state == autonomous) {
+    
+    // check to see if it's time to blink the LED; that is, if the
+    // difference between the current time and last time you blinked
+    // the LED is bigger than the interval at which you want to
+    // blink the LED.
+    unsigned long currentMillis = millis();
+  
+    if (currentMillis - previousMillis >= BLINK_INTERVAL) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+  
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+  
+      // set the LED with the ledState of the variable:
+      digitalWrite(ledPin, ledState);
+    }
+    
     while (Serial.available() > 0) {
       // Read the next character
       chr = Serial.read();
